ae09e7a14d70ac1363cf0d3291df5bef8139fe2c
wanghui <793147654@qq.com>
2019-09-05 22:23:01 +0800

    完善确认收货

--- src/main/resources/db/migration/V1__init_employee_table.sql
+++ src/main/resources/db/migration/V1__init_employee_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE express (
-  id VARCHAR(64) NOT NULL,
+  id VARCHAR(64) NOT NULL unique ,
   name VARCHAR(32) NOT NULL,
   phone VARCHAR(32) NOT NULL,
   status VARCHAR(32) NOT NULL,
@@ -11,10 +11,10 @@ insert into `express`
 values('1','小明','123455','未取件','2019-09-05 16:20:51');
 
 insert into `express`
-values('2','小红','123455','未取件','2019-09-05 16:20:51');
+values('2','小红','123455','未取件','2019-09-05 16:20:52');
 
 insert into `express`
-values('3','小牛','123455','未取件','2019-09-05 16:20:51');
+values('3','小牛','123455','未取件','2019-09-05 16:20:53');
 
 insert into `express`
-values('4','小张','123455','未取件','2019-09-05 16:20:51');
+values('4','小张','123455','未取件','2019-09-05 16:20:54');
